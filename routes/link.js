6253f4883da8c1ee7f452a294ef32533a71676f5
kkopoku <kk.opoku@outlook.com>
2025-01-02 03:42:30 +0000

    chore: added test route

--- routes/link.js
+++ routes/link.js
@@ -1,7 +1,8 @@
-const {createLink, getLinkDetails} = require('../controller/link')
+const {createLink, getLinkDetails, testing} = require('../controller/link')
 const router = require('express').Router()
 
 router.post('/createLink', createLink)
 router.get('/getLinkDetails', getLinkDetails)
+router.get('/test', testing)
 
 module.exports = router
