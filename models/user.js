166afb5228f6c384ab2e3d58d20f3db29f5c8b1f
kkopoku <kk.opoku@outlook.com>
2025-01-12 12:19:41 +0000

    add: created user model

---
+++ models/user.js
@@ -0,0 +1,59 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt"
+
+
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    otherNames: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+
+userSchema.methods.createJWT = () => {
+    return jwt.sign(
+        {
+            id: this.id,
+            firstName: this.firstName,
+            otherNames: this.otherNames,
+            email: this.email,
+            password: this.password
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_LIFETIME }
+    );
+};
+
+
+userSchema.methods.comparePassword = async (candidatePassword) => {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+};
+
+
+userSchema.pre('save', async function (next) {
+    try {
+        if (this.isModified('password')) {
+            const salt = await bcrypt.genSalt(10);
+            this.password = bcrypt.hash(this.password, salt);
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+const UserModel = model('User', UserSchema)
+export default UserModel
