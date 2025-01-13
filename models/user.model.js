b4cb9b709643ead02cb5acc5aba0779ba1eb0f09
kkopoku <kk.opoku@outlook.com>
2025-01-13 16:41:43 +0000

    fix: addded await

--- models/user.model.js
+++ models/user.model.js
@@ -52,7 +52,7 @@ UserSchema.pre('save', async function (next) {
     try {
         if (this.isModified('password')) {
             const salt = await bcrypt.genSalt(10);
-            this.password = bcrypt.hash(this.password, salt);
+            this.password = await bcrypt.hash(this.password, salt);
         }
         next();
     } catch (error) {
