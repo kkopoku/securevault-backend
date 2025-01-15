2f68e61280bfb155509212740395ebd15ec64adf
kkopoku <kk.opoku@outlook.com>
2025-01-15 00:02:10 +0000

    chore: renamed

---
+++ jobs/link-created.job.js
@@ -0,0 +1,48 @@
+const dotenv = require("dotenv")
+const Bull = require("bull");
+const Analytics = require("../models/analytic.model")
+
+dotenv.config()
+const { REDIS_HOST, REDIS_PORT, UPSTASH_ENDPOINT, UPSTASH_PASSWORD, UPSTASH_PORT, APP } = process.env
+
+const localConfig = {
+    host: REDIS_HOST,
+    port: Number(REDIS_PORT)
+}
+
+const prodConfig = {
+    url: `rediss://default:${UPSTASH_PASSWORD}@${UPSTASH_ENDPOINT}:${UPSTASH_PORT}`,
+}
+
+const redisConfig = (APP == "local") ? localConfig : prodConfig
+
+const linkCreatedQueue = new Bull("linkCreatedQueue", {
+    redis: redisConfig
+});
+
+
+linkCreatedQueue.process(async (data) => {
+    const tag = "[analytic.job.js][process]"
+    console.log(`${tag} Processing link job`)
+
+    try{
+        await Analytics.create({ type: "Link" })
+    }catch(error){
+        console.log(`${tag} Failed to write to DB`)
+    }
+
+});
+
+linkCreatedQueue.on("error", (err) => {
+    console.error("Redis connection error:", err);
+});
+
+linkCreatedQueue.on("stalled", (job) => {
+    console.warn(`Job ${job.id} stalled and will be retried`);
+});
+
+linkCreatedQueue.on("failed", async (job, error) => {
+    console.error(`Job ${job.id} failed with error: ${error.message}`);
+});
+
+module.exports = linkCreatedQueue;
